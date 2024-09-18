272e9cd83f026dd5a194555146085512673b0714
Wafaa Mohamed <121331529+Wafaamohamed2@users.noreply.github.com>
2024-09-18 22:13:58 +0300

    Add project files.

---
+++ EF- Index/ApplicationDbContext.cs
@@ -0,0 +1,85 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewEF
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+        
+            options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EFCore; Integrated Security = True");
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // make the value of the index is unique using flount API and give it name
+            modelBuilder.Entity<Blog>()
+                   .HasIndex(b => b.Url)
+                   .IsUnique()
+                   .HasDatabaseName("Index_Url_Test")
+                   .HasFilter(null);
+
+
+
+            //modelBuilder.Entity<Person>()
+            //    .HasIndex(p => new { p.FristName, p.LastName });
+
+
+        }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Blog> Tags { get; set; }
+        public DbSet<Person> Persons { get; set; } 
+
+    }
+
+
+   
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public int BlogId { get; set; }
+
+        public Blog Blog { get; set; }
+
+
+
+    }
+    // make the value of the index is unique using data anotation and give it name
+
+   // [Index(nameof(Url) , IsUnique = true,  Name = "Index_Url")]
+    public class Blog 
+    {
+        public int BlogId { get; set; }
+
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; set; }   
+
+    }
+    //Composite Index
+
+     [Index(nameof(FristName) , nameof(LastName))]
+
+    public class Person
+    {
+        [Key]
+        public int PostId { get; set; }
+
+        public String FristName { get; set; }
+
+        public String LastName { get; set; }
+
+
+
+    }
+}
---
+++ EF- Index/Migrations/20240918191143_GiveIndexName.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF__Index.Migrations
+{
+    /// <inheritdoc />
+    public partial class GiveIndexName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FristName = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.PostId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    BlogId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.BlogId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BlogId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                    table.ForeignKey(
+                        name: "FK_Posts_Tags_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Tags",
+                        principalColumn: "BlogId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_FristName_LastName",
+                table: "Persons",
+                columns: new[] { "FristName", "LastName" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_BlogId",
+                table: "Posts",
+                column: "BlogId");
+
+            migrationBuilder.CreateIndex(
+                name: "Index_Url_Test",
+                table: "Tags",
+                column: "Url",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
